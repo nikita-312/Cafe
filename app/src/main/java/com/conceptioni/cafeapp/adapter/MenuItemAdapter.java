e3618e8318ed03f353509474c3cbf4138d7181ce
mona <mona.conceptioni@gmail.com>
2018-06-29 10:53:57 +0530

    commit by nikita 29/6

--- app/src/main/java/com/conceptioni/cafeapp/adapter/MenuItemAdapter.java
+++ app/src/main/java/com/conceptioni/cafeapp/adapter/MenuItemAdapter.java
@@ -1,13 +1,16 @@
 package com.conceptioni.cafeapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.conceptioni.cafeapp.R;
+import com.conceptioni.cafeapp.activity.DescriptionActivity;
 
 public class MenuItemAdapter extends RecyclerView.Adapter<MenuItemAdapter.MenuViewHolder> {
 
@@ -29,7 +32,9 @@ public class MenuItemAdapter extends RecyclerView.Adapter<MenuItemAdapter.MenuVi
 
     @Override
     public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
-
+        holder.itemll.setOnClickListener(v -> {
+            context.startActivity(new Intent(context, DescriptionActivity.class));
+        });
     }
 
     @Override
@@ -38,8 +43,10 @@ public class MenuItemAdapter extends RecyclerView.Adapter<MenuItemAdapter.MenuVi
     }
 
     public class MenuViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout itemll;
         public MenuViewHolder(View itemView) {
             super(itemView);
+            itemll = itemView.findViewById(R.id.itemll);
         }
     }
 }
