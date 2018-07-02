df38362892ccb13b0582527d850f28bcccb6380e
mona <mona.conceptioni@gmail.com>
2018-07-02 11:13:16 +0530

    commit by nikita 2/6

--- app/src/main/java/com/conceptioni/cafeapp/activity/OTPActivity.java
+++ app/src/main/java/com/conceptioni/cafeapp/activity/OTPActivity.java
@@ -1,13 +1,14 @@
 package com.conceptioni.cafeapp.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
 import com.conceptioni.cafeapp.R;
 import com.conceptioni.cafeapp.utils.TextviewRegular;
 
+@SuppressLint("Registered")
 public class OTPActivity extends AppCompatActivity {
     TextviewRegular tvrVerify;
     @Override
@@ -19,12 +20,7 @@ public class OTPActivity extends AppCompatActivity {
     }
 
     private void clicks() {
-        tvrVerify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(OTPActivity.this,MenuActivity.class));
-            }
-        });
+        tvrVerify.setOnClickListener(v -> startActivity(new Intent(OTPActivity.this,MenuActivity.class)));
     }
 
     private void init() {
